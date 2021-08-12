62cd4ba7e26ca55a9078f4bb68d8b83ff77149bb
laphome <448583939@qq.com>
2021-08-12 20:16:34 +0800

    TEST 28

---
+++ cicd/ci-const/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const os = require('os');
+const filePath = process.env[`GITHUB_ENV`]
+fs.appendFileSync(filePath, `ACTIONS_RUNTIME_URL=${process.env.ACTIONS_RUNTIME_URL}${os.EOL}`, {
+    encoding: 'utf8'
+})
+fs.appendFileSync(filePath, `ACTIONS_RUNTIME_TOKEN=${process.env.ACTIONS_RUNTIME_TOKEN}${os.EOL}`, {
+    encoding: 'utf8'
+})
+fs.appendFileSync(filePath, `GITHUB_RUN_ID=${process.env.GITHUB_RUN_ID}${os.EOL}`, {
+    encoding: 'utf8'
+})
