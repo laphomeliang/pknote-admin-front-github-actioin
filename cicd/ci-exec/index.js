b8da46ac7c047b05cbe7df4f054236bc371c656f
laphome <448583939@qq.com>
2021-08-14 22:45:18 +0800

    test64

--- cicd/ci-exec/index.js
+++ cicd/ci-exec/index.js
@@ -6,7 +6,7 @@ try {
     io.which('git', true).then((gitPath) => {
         core.info(gitPath)
         core.info(process.env.GITHUB_SHA)
-        const args = ['diff', '--name-only', `1ece854ab7f5174a83ee2eefadd6e67fbd9e277d`, `${process.env.GITHUB_SHA}`]
+        const args = ['diff', '--name-only', `1d78bf20b45e36a034e5bd5e5b4e18d665f180c2`, `${process.env.GITHUB_SHA}`]
         // const args = ['log']
         exec.exec(`"${gitPath}"`, args).then((pathArr) => {
             core.info(pathArr)
