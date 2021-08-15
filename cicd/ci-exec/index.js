dd7da051ee3bbd1fa87db4e7adf6b807df9aab0a
laphome <448583939@qq.com>
2021-08-15 14:13:01 +0800

    test65

--- cicd/ci-exec/index.js
+++ cicd/ci-exec/index.js
@@ -1,12 +1,14 @@
+const fs = require('fs')
 const exec = require('@actions/exec')
 const io = require('@actions/io')
 const core = require('@actions/core')
 
 try {
+    core.info(fs.exists('./.git'))
     io.which('git', true).then((gitPath) => {
         core.info(gitPath)
         core.info(process.env.GITHUB_SHA)
-        const args = ['diff', '--name-only', `1d78bf20b45e36a034e5bd5e5b4e18d665f180c2`, `${process.env.GITHUB_SHA}`]
+        const args = ['diff', '--name-only']
         // const args = ['log']
         exec.exec(`"${gitPath}"`, args).then((pathArr) => {
             core.info(pathArr)
