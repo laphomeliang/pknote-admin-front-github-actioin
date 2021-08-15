bc9e1179f431fb1fd4bf60b8626dc66101523fed
laphome <448583939@qq.com>
2021-08-15 14:30:30 +0800

    test68

--- cicd/ci-exec/index.js
+++ cicd/ci-exec/index.js
@@ -5,17 +5,29 @@ const core = require('@actions/core')
 
 try {
     core.info('48484')
-    core.info(fs.exists('./.git'))
-    io.which('git', true).then((gitPath) => {
-        core.info('gitPath')
-        core.info(gitPath)
-        core.info(process.env.GITHUB_SHA)
-        const args = ['diff', '--name-only']
-        // const args = ['log']
-        exec.exec(`"${gitPath}"`, args).then((pathArr) => {
-            core.info(pathArr)
-        })
-    })
+    const gitPath = getGit()
+    core.info(gitPath)
+    const args = ['diff', '--name-only']
+    const pathArr = execFunc(gitPath, args)
+    core.info(pathArr)
+    // io.which('git', true).then((gitPath) => {
+    //     core.info('gitPath')
+    //     core.info(gitPath)
+    //     core.info(process.env.GITHUB_SHA)
+    //     const args = ['diff', '--name-only']
+    //     // const args = ['log']
+    //     exec.exec(`"${gitPath}"`, args).then((pathArr) => {
+    //         core.info(pathArr)
+    //     })
+    // })
 } catch (error) {
     
+}
+
+async function getGit() {
+    return await io.which('git', true)
+}
+
+async function execFunc(gitPath, args) {
+    return await exec.exec(gitPath, args)
 }
