6043eadb9a1cf48aaf978628aa7d92db8a2d291b
laphome <448583939@qq.com>
2021-08-21 22:54:58 +0800

    test100

--- cicd/cd-translation/index.js
+++ cicd/cd-translation/index.js
@@ -1,9 +1,31 @@
+const exec = require('@actions/exec')
+const io = require('@actions/io')
 const core = require('@actions/core')
 
 try {
     new Promise(async function (resolve, reject) {
-        core.info(`${ process.env.COMMIT_PATHS }`)
+        const gitPath = await io.which('git', true)
+        const gitFilePath = `"${gitPath}"`
+        const args2 = ['diff', '--name-only', 'HEAD~', 'HEAD']
+        let myOutput = ''
+        const options = {
+            cwd: `${ process.env.GITHUB_REPOSITORY }`,
+            listeners: {
+                stdout: data => {
+                    myOutput = data.toString();
+                }
+            }
+        }
+        await exec.exec(gitFilePath, args2, options)
+        core.info('get the commit file paths!')
+        splitPaths(myOutput)
     })
-} catch (error) {
+} catch (error) {}
+
+function splitPaths(str) {
+    const arr = str.split(/\s+/g)
+    arr.forEach((s, i) => {
+        core.info(`${ i }, ${ s }`)
+    });
     
 }
--- cicd/ci-checkout/index.js
+++ cicd/ci-checkout/index.js
@@ -1,33 +1,12 @@
-const fs = require('fs')
-const os = require('os');
-const filePath = process.env[`GITHUB_ENV`]
 const exec = require('@actions/exec')
 const io = require('@actions/io')
 const core = require('@actions/core')
 try {
     new Promise(async function (resolve, reject) {
         const gitPath = await io.which('git', true)
-        console.log(gitPath)
         const args1 = ['clone', `https://github.com/${ process.env.GITHUB_REPOSITORY }`, `${ process.env.GITHUB_REPOSITORY }`]
         const gitFilePath = `"${gitPath}"`
         await exec.exec(gitFilePath, args1)
-        setTimeout(async () => {
-            const args2 = ['diff', '--name-only', 'HEAD~', 'HEAD']
-            let myOutput = ''
-            const options = {
-                cwd: `${ process.env.GITHUB_REPOSITORY }`,
-                listeners: {
-                    stdout: data => {
-                        myOutput = data.toString();
-                    }
-                }
-            }
-            await exec.exec(gitFilePath, args2, options)
-            core.info(':rocket: clone code from repository success and get the commited path!')
-            core.info(myOutput)
-            fs.appendFileSync(filePath, `COMMIT_PATHS=${myOutput}${os.EOL}`, {
-                encoding: 'utf8'
-            })
-        }, 2000);
+        core.info(':rocket: clone code from repository success!')
     })
 } catch (error) {}
