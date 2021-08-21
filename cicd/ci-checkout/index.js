469ce46fe749b332a5df89d262eeec3d47f83b4d
laphome <448583939@qq.com>
2021-08-21 20:31:53 +0800

    test86

--- cicd/ci-checkout/index.js
+++ cicd/ci-checkout/index.js
@@ -7,13 +7,13 @@ try {
     new Promise(async function (resolve, reject) {
         const gitPath = await io.which('git', true)
         console.log(gitPath)
-        const args1 = ['clone', `https://github.com/${ process.env.GITHUB_REPOSITORY }`, './public/action'] // github.repository
+        const args1 = ['clone', `https://github.com/${ process.env.GITHUB_REPOSITORY }`, './public/action']
         const gitFilePath = `"${gitPath}"`
         await exec.exec(gitFilePath, args1)
         setTimeout(async () => {
             const args2 = ['diff', '--name-only', 'HEAD~', 'HEAD']
             await exec.exec(gitFilePath, args2, { cwd: './public/action' })
             core.info(':rocket: clone code from repository success!')
-        }, 300000);
+        }, 20000);
     })
 } catch (error) {}
