e1497d740e3de069d373ddc90f2b84ce8e92e0da
laphome <448583939@qq.com>
2021-08-28 16:12:29 +0800

    test129

--- cicd/translation/uils.js
+++ cicd/translation/uils.js
@@ -5,6 +5,7 @@ const isConnect = (txt, newObj, tempObj) => {
     const { index: nIndex, txt: nTxt } = newObj
     const { index: tIndex, txt: tTxt } = tempObj
     const inTxt = txt.substring(tIndex + tTxt.length, nIndex)
+    core.info('inTxt')
     core.info(inTxt)
     let connected = false
     const regRN = /\r\n/g
