374b13cd6ff1117e67ab1974298ed1815f83a985
laphome <448583939@qq.com>
2021-09-06 21:15:09 +0800

    test176

--- cicd/ci-checkout/index.js
+++ cicd/ci-checkout/index.js
@@ -4,7 +4,7 @@ const core = require('@actions/core')
 try {
     new Promise(async function (resolve, reject) {
         const gitPath = await io.which('git', true)
-        const args1 = ['clone', `https://github.com/${ process.env.GITHUB_REPOSITORY }`, `${ process.env.GITHUB_REPOSITORY }`]
+        const args1 = ['clone', `https://github.com/${ process.env.GITHUB_REPOSITORY }`, `${ process.env.GIT_CLONE_PATH }/${ process.env.GITHUB_REPOSITORY }`]
         const gitFilePath = `"${gitPath}"`
         await exec.exec(gitFilePath, args1)
         core.info('🚀 clone code from repository success!')
