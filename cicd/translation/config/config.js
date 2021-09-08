51ac028062588c39054f5cdeed47e2cab8bcb7fa
laphome <448583939@qq.com>
2021-09-08 19:44:51 +0800

    test197

--- cicd/translation/config/config.js
+++ cicd/translation/config/config.js
@@ -1,4 +1,4 @@
-const auth_keys = ["5652c0b9-adcf-7f2e-f6a2-3a577f700dc9:fx"]
+const auth_keys = ["9c6bb53c-be1b-4a78-63f6-5e225a954c97:fx", "5652c0b9-adcf-7f2e-f6a2-3a577f700dc9:fx"]
 const lang_dir = 'src/lang'
 const lang_en = 'en-US.js'
 const lang_zh = 'zh-CN.js'
